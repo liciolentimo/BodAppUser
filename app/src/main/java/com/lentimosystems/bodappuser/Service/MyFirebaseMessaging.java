9561105b54d1544a0fee38359ca93b346daad069
Licio Lentimo <lentimo@gmail.com>
2018-02-05 18:37:03 +0300

    Notification when driver arrives.

--- app/src/main/java/com/lentimosystems/bodappuser/Service/MyFirebaseMessaging.java
+++ app/src/main/java/com/lentimosystems/bodappuser/Service/MyFirebaseMessaging.java
@@ -1,13 +1,19 @@
 package com.lentimosystems.bodappuser.Service;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Handler;
 import android.os.Looper;
+import android.support.v4.app.NotificationCompat;
 import android.widget.Toast;
 
 import com.google.android.gms.maps.model.LatLng;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
+import com.lentimosystems.bodappuser.R;
 
 /**
  * Created by LICIO on 1/15/2018.
@@ -16,13 +22,38 @@ import com.google.firebase.messaging.RemoteMessage;
 public class MyFirebaseMessaging extends FirebaseMessagingService {
     @Override
     public void onMessageReceived(final RemoteMessage remoteMessage) {
-        //choose hander from android.os
-        Handler handler = new Handler(Looper.getMainLooper());
-        handler.post(new Runnable() {
-            @Override
-            public void run() {
-                Toast.makeText(MyFirebaseMessaging.this, ""+remoteMessage.getNotification().getBody(), Toast.LENGTH_SHORT).show();
-            }
-        });
+        if (remoteMessage.getNotification().getTitle().equals("Cancel"))
+        {
+            //choose hander from android.os
+            Handler handler = new Handler(Looper.getMainLooper());
+            handler.post(new Runnable() {
+                @Override
+                public void run() {
+                    Toast.makeText(MyFirebaseMessaging.this, ""+remoteMessage.getNotification().getBody(), Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+        else if (remoteMessage.getNotification().getTitle().equals("Arrived"))
+        {
+           showArrivedNotification(remoteMessage.getNotification().getBody());
+        }
     }
+
+    private void showArrivedNotification(String body) {
+        //this only works for API 25 and below, for 26 and above you need a notification channel
+        PendingIntent contentIntent = PendingIntent.getActivity(getBaseContext(),
+                0,new Intent(),PendingIntent.FLAG_ONE_SHOT);
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(getBaseContext());
+
+        builder.setAutoCancel(true)
+                .setDefaults(Notification.DEFAULT_LIGHTS|Notification.DEFAULT_SOUND)
+                .setWhen(System.currentTimeMillis())
+                .setSmallIcon(R.mipmap.ic_launcher_round)
+                .setContentTitle("Arrived")
+                .setContentText(body)
+                .setContentIntent(contentIntent);
+        NotificationManager manager = (NotificationManager)getBaseContext().getSystemService(Context.NOTIFICATION_SERVICE);
+        manager.notify(1,builder.build());
+    }
+
 }
