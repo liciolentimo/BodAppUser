de238ee0e469821b51c1226aee93ffac32403923
Licio Lentimo <lentimo@gmail.com>
2018-03-12 20:32:02 +0300

    Gets fare estimate.

--- app/src/main/java/com/lentimosystems/bodappuser/BottomSheetRiderFragment.java
+++ app/src/main/java/com/lentimosystems/bodappuser/BottomSheetRiderFragment.java
@@ -3,10 +3,22 @@ package com.lentimosystems.bodappuser;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.design.widget.BottomSheetDialogFragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+
+import com.lentimosystems.bodappuser.Common.Common;
+import com.lentimosystems.bodappuser.Remote.IGoogleAPI;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 
 /**
@@ -15,6 +27,9 @@ import android.widget.TextView;
 
 public class BottomSheetRiderFragment extends BottomSheetDialogFragment {
     String mLocation,mDestination;
+
+    IGoogleAPI mService;
+    TextView txtCalculate;
 
     public static BottomSheetRiderFragment newInstance(String location,String destination)
     {
@@ -39,10 +54,70 @@ public class BottomSheetRiderFragment extends BottomSheetDialogFragment {
         View view = inflater.inflate(R.layout.bottom_sheet_rider,container,false);
         TextView txtLocation = (TextView)view.findViewById(R.id.txtLocation);
         TextView txtDestination = (TextView)view.findViewById(R.id.txtDestination);
-        TextView txtCalculate = (TextView)view.findViewById(R.id.txtCalculate);
+        txtCalculate = (TextView)view.findViewById(R.id.txtCalculate);
 
+        mService = Common.getGoogleService();
+        getPrice(mLocation,mDestination);
         txtLocation.setText(mLocation);
         txtDestination.setText(mDestination);
         return view;
+    }
+
+    private void getPrice(String mLocation, String mDestination) {
+        String requestUrl = null;
+        try {
+            requestUrl = "https://maps.googleapis.com/maps/api/directions/json?"+
+                    "mode=driving&"
+                    +"transit_routing_preference=less_driving&"
+                    +"origin="+mLocation+"&"
+                    +"destination="+mDestination+"&"
+                    +"key="+getResources().getString(R.string.google_direction_api);
+            Log.e("LINK",requestUrl);
+            mService.getPath(requestUrl).enqueue(new Callback<String>() {
+                @Override
+                public void onResponse(Call<String> call, Response<String> response) {
+                    try {
+                        JSONObject jsonObject = new JSONObject(response.body().toString());
+                        JSONArray routes = jsonObject.getJSONArray("routes");
+
+                        JSONObject object = routes.getJSONObject(0);
+                        JSONArray legs = object.getJSONArray("legs");
+
+                        JSONObject legsObject = legs.getJSONObject(0);
+
+                        //Get distance
+                        JSONObject distance = legsObject.getJSONObject("distance");
+                        String distance_text = distance.getString("text");
+
+                        //regex to remove text that is not in digit form
+                        Double distance_value = Double.parseDouble(distance_text.replaceAll("[^0-9\\\\.]+",""));
+
+                        //Get time
+                        JSONObject time = legsObject.getJSONObject("duration");
+                        String time_text = time.getString("text");
+                        Integer time_value = Integer.parseInt(time_text.replaceAll("\\D+",""));
+
+                        String final_calculation = String.format("%s + %s = $%.2f",distance_text,time_text,
+                                                    Common.getPrice(distance_value,time_value));
+
+                        txtCalculate.setText(final_calculation);
+
+
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+
+                }
+
+                @Override
+                public void onFailure(Call<String> call, Throwable t) {
+                    Log.e("ERROR",t.getMessage());
+                }
+            });
+        }
+        catch (Exception ex)
+        {
+            ex.printStackTrace();
+        }
     }
 }
--- app/src/main/java/com/lentimosystems/bodappuser/Common/Common.java
+++ app/src/main/java/com/lentimosystems/bodappuser/Common/Common.java
@@ -1,7 +1,9 @@
 package com.lentimosystems.bodappuser.Common;
 
 import com.lentimosystems.bodappuser.Remote.FCMClient;
+import com.lentimosystems.bodappuser.Remote.GoogleMapsAPI;
 import com.lentimosystems.bodappuser.Remote.IFCMService;
+import com.lentimosystems.bodappuser.Remote.IGoogleAPI;
 
 /**
  * Created by LICIO on 1/14/2018.
@@ -15,9 +17,23 @@ public class Common {
     public static final String token_tbl = "Tokens";
 
     public static final String fcmURL = "https://fcm.googleapis.com";
+    public static final String googleApiUrl = "https://maps.googleapis.com";
+
+    private static double base_fare = 2.55;
+    private static double time_rate = 0.35;
+    private static double distance_rate = 1.75;
+
+    public static double getPrice(double km, int min)
+    {
+        return (base_fare+(time_rate*min)+(distance_rate*km));
+    }
 
     public static IFCMService getFCMService()
     {
         return FCMClient.getClient(fcmURL).create(IFCMService.class);
+    }
+    public static IGoogleAPI getGoogleService()
+    {
+        return GoogleMapsAPI.getClient(googleApiUrl).create(IGoogleAPI.class);
     }
 }
